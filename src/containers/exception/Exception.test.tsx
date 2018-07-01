2294b9a3cb8e2d73e8bc636bb69359ede61015b3
Louis Vo <sonvh>
2018-07-01 11:24:56 +0700

    Provide some unit test for containers

---
+++ src/containers/exception/Exception.test.tsx
@@ -0,0 +1,9 @@
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import Exception from './index';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Exception />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
---
+++ src/containers/landing/Landing.test.tsx
@@ -0,0 +1,9 @@
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import Landing from './index';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Landing />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
